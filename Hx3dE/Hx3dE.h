985b20bca88f6faca4d4db7ab5b2c2e61781cdaa
Hexagons <anton.heestand@gmail.com>
2018-08-02 21:07:54 +0200

    Initial Commit

---
+++ Hx3dE/Hx3dE.h
@@ -0,0 +1,19 @@
+//
+//  Hx3dE.h
+//  Hx3dE
+//
+//  Created by Hexagons on 2018-08-02.
+//  Copyright © 2018 Hexagons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hx3dE.
+FOUNDATION_EXPORT double Hx3dEVersionNumber;
+
+//! Project version string for Hx3dE.
+FOUNDATION_EXPORT const unsigned char Hx3dEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hx3dE/PublicHeader.h>
+
+
