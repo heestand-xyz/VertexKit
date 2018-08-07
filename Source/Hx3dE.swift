35214bed9b45ecd41864991b48b658b51782bbe2
Hexagons <anton.heestand@gmail.com>
2018-08-07 11:19:54 +0200

    updates HxH Signature

--- Source/Hx3dE.swift
+++ Source/Hx3dE.swift
@@ -10,6 +10,31 @@ import Foundation
 
 public class Hx3dE {
     
-    public static let engine: _3DEngine = _3DScnEngine()
+    public static let main = Hx3dE()
+    
+    public let engine: _3DEngine = _3DScnEngine()
+    
+    let kSlug = "Hx3dE"
+    let kName = "Hexagon 3D Engine"
+    let kBundleId = "house.hexagon.hx3de"
+    
+    struct HxHSignature: Encodable {
+        let slug: String
+        let name: String
+        let id: String
+        let version: Float
+        let build: Int
+        var formatted: String {
+            return "\(slug) - \(name) - \(id) - v\(version) - b\(build)"
+        }
+    }
+    
+    var hxhSignature: HxHSignature {
+        return HxHSignature(slug: kSlug, name: kName, id: kBundleId, version: Float(Bundle(identifier: kBundleId)!.infoDictionary?["CFBundleShortVersionString"] as? String ?? "-1")!, build: Int(Bundle(identifier: kBundleId)!.infoDictionary?["CFBundleVersion"] as? String ?? "-1")!)
+    }
+    
+    init() {
+        print(hxhSignature.formatted)
+    }
     
 }
