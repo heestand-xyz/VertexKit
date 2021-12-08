242a55c40341b8ae16a990701756d42fae7b6ef8
Anton Heestand <anton.heestand@gmail.com>
2021-12-08 10:14:19 +0100

    Tags

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "VertexKit", targets: ["VertexKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/PixelKit", from: "2.1.1"),
+        .package(url: "https://github.com/heestand-xyz/PixelKit", from: "2.2.1"),
         .package(url: "https://github.com/heestand-xyz/PixelColor", from: "1.3.2"),
         .package(url: "https://github.com/heestand-xyz/Resolution", from: "1.0.4"),
         .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.2.1"),
