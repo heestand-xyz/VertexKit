2be56472a4fb43ce851dd7875864772b6b5c7fe3
Anton Heestand <anton.heestand@gmail.com>
2022-06-04 18:26:10 +0200

    CoreGraphicsExtensions

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/heestand-xyz/PixelKit", from: "3.0.1"),
         .package(url: "https://github.com/heestand-xyz/PixelColor", from: "1.3.2"),
         .package(url: "https://github.com/heestand-xyz/Resolution", from: "1.0.4"),
-        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.2.1"),
+        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.3.2"),
     ],
     targets: [
         .target(name: "VertexKit", dependencies: ["PixelKit", "PixelColor", "Resolution", "CoreGraphicsExtensions"], path: "Source"),
